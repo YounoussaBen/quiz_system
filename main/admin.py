5ef4cb91e3fba8ecbec69f6a8fbd401249849eea
Younoussa Ben <younoussaabdourhaman@gmail.com>
2024-09-01 21:24:00 +0000

    admin

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User, Badge
+from .models import User, Badge, Quiz
 
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Badge)
+admin.site.register(Quiz)
