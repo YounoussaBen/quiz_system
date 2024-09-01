b8ffdb9cde05bc4e1664d8161905ad4435a5b80c
Younoussa Ben <younoussaabdourhaman@gmail.com>
2024-09-01 21:24:16 +0000

    urls

--- main/urls.py
+++ main/urls.py
@@ -14,7 +14,17 @@ urlpatterns = [
     path('admin_quizzes/<int:quiz_id>/edit/', views.edit_quiz, name='edit_quiz'),
     path('admin_quizzes/<int:quiz_id>/delete/', views.delete_quiz, name='delete_quiz'), 
     path('admin_user_statistics/', views.user_statistics, name='user_statistics'),
-    path('admin_user_profile/<int:user_id>/', views.user_profile, name='user_profile'),
+    path('admin_user_profile/<int:user_id>/', views.admin_profile, name='admin_profile'),
 
-    # path('dashboard/', user_dashboard, name='user_dashboard'),
+    # path('dashboard/', user_home, name='user_home'),
+    path('dashboard/', views.user_home, name='user_home'),
+    path('quizzes/', views.quizzes, name='quizzes'),
+    path('quiz/start/<int:quiz_id>/', views.start_quiz, name='start_quiz'),
+    path('quiz/<int:attempt_id>/', views.take_quiz, name='take_quiz'),
+    path('quiz/continue/<int:attempt_id>/', views.continue_quiz, name='continue_quiz'),
+    path('leaderboard/', views.leaderboard_view, name='leaderboard'),
+    path('my_profile/', views.my_profile, name='my_profile'),
+
+
+
 ]
